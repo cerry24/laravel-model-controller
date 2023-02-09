0296b288ed427671603e68efff479c24618193b0
cerry24 <samuelecerretti@gmail.com>
2023-02-09 17:39:27 +0100

    get movies from db & print them

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,23 @@
 <body>
 
     <main class="bg-light">
-        
+        <div class="container pt-5">
+            <div class="row justify-content-around gy-5">
+                @forelse ($movies as $movie)
+                    <div class="col-5">
+                        <article class="card p-3">
+                            <h3 class="text-primary fw-bold text-uppercase">{{ $movie->title }}</h3>
+                            <h4>{{ $movie->original_title }}</h4>
+                            <p>Nationality: {{ $movie->nationality }}</p>
+                            <p>Release date: {{ $movie->date }}</p>
+                            <p>Rating: {{ $movie->vote }}/10</p>
+                        </article>
+                    </div>
+                @empty
+                    <p>0 movies found</p>
+                @endforelse
+            </div>
+        </div>
     </main>
 
 </body>
